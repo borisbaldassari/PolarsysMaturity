8054766da261febc74a447305896d31f27b73885
Boris Baldassari <boris.baldassari@gmail.com>
2014-12-11 15:17:03 +0100

    #9 add retrieval script for sonarqube metrics.

---
+++ scripts/retrieve_data/retrieve_data_sonarqube.pl
@@ -0,0 +1,35 @@
+#! perl
+#
+#
+#
+#
+#
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use JSON qw( encode_json );
+use XML::LibXML;
+use LWP::Simple;
+
+# This can be changed for more verbose output
+my $debug = 1;
+my $base_url = "http://projects.eclipse.org/json/project/";
+
+die "Usage: $0 project_id dir_out\n" if (scalar @ARGV != 2);
+
+my $project_id = shift;
+my $dir_out = shift;
+
+my $url = "http://dashboard.eclipse.org/data/json/" 
+    . $project_id 
+    . "-sonar-prj-static.json";
+my $file_out = $dir_out . "/" . $project_id . "_metrics_sonarqube.json";
+
+# Fetch json file from the dashboard.eclipse.org
+print "Fetching file from [$url].\n";
+my $content = getstore($url, $file_out);
+die "Could not get [$url]!" unless defined $content;
+ 
+print "Finished importing data from SonarQube.\n";
